b6d6afef04d470e39a8f5acbfa08ff3b6022fe0c
tanzilmia <ziltan590@gmail.com>
2022-11-25 19:30:58 +0600

    add selller and bayer

--- src/Pages/DashBoardPage/AllByer/AllByer.js
+++ src/Pages/DashBoardPage/AllByer/AllByer.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 const AllByer = () => {
-    return (
-        <div>
-            <h2> All Byers Page </h2>
-        </div>
-    );
+  const { data: bayers = [], isLoading } = useQuery({
+    queryKey: [""],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/bayer`);
+      const data = await res.json();
+      return data;
+    },
+  });
+  return (
+    <div className="overflow-x-auto">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bayers.map((bayer, index) => (
+            <tr key={bayer._id}>
+              <th>{index + 1}</th>
+              <th> {bayer.name} </th>
+              <th>{bayer.email}</th>
+              <th><button>Delete</button></th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default AllByer;
--- src/Pages/DashBoardPage/AllSeller/Allseller.js
+++ src/Pages/DashBoardPage/AllSeller/Allseller.js
@@ -1,10 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
+
 const Allseller = () => {
+
+    const {data : sellers = [], isLoading} = useQuery({
+        queryKey : [''],
+        queryFn : async ()=>{
+            const res = await fetch(`http://localhost:5000/seller`)
+            const data = await res.json()
+            return data
+        }
+    })
+
     return (
-        <div>
-            <h2>All Seller Page </h2>
-        </div>
+        <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellers.map((seller, index) => (
+              <tr key={seller._id}>
+                <th>{index + 1}</th>
+                <th> {seller.name} </th>
+                <th>{seller.email}</th>
+                <th><button className='btn btn-sm btn-primary'>Verify</button></th>
+                <th><button className='btn btn-sm btn-warning'>Delete</button></th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
 };
 
